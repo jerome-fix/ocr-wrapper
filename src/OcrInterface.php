1aba14794e997c77bb16a1bb51afeb1c39d27def
Jérôme FIX <jerome.fix@zapoyok.info>
2015-09-21 07:05:15 +0200

    Add interface

--- src/OcrInterface.php
+++ src/OcrInterface.php
@@ -18,4 +18,80 @@ interface OcrInterface
      * or execute the scan.
      */
     public function cleanup();
+
+    /**
+     * @param \SplFileInfo $file
+     *
+     * @return BaseOcr
+     */
+    public function setFile(\SplFileInfo $file);
+
+    /**
+     * @return \SplFileInfo
+     */
+    public function getFile();
+
+    public function buildCommand();
+
+    public function scan();
+
+    /**
+     * @return string
+     */
+    public function getBinary();
+
+    /**
+     * @param string $binary
+     *
+     * @return BaseOcr
+     */
+    public function setBinary($binary);
+
+    /**
+     * @return int
+     */
+    public function getTimeout();
+
+    /**
+     * @param int $timeout
+     *
+     * @return BaseOcr
+     */
+    public function setTimeout($timeout);
+
+    /**
+     * @return string
+     */
+    public function getLanguages();
+
+    /**
+     * @param array $languages
+     *
+     * @return BaseOcr
+     */
+    public function setLanguages($languages);
+
+    /**
+     * @param string $language
+     *
+     * @return BaseOcr
+     */
+    public function addLanguage($language);
+
+    /**
+     * @param $dir
+     *
+     * @throws \Jfx\Ocr\Exception\DirectoryNotFoundException
+     *
+     * @return $this
+     */
+    public function setTmpDir($dir);
+
+    /**
+     * Returns the path of a directory to place temporary files if defined,
+     * otherwise returns the default temp directory of the operating system.
+     *
+     * @return string
+     */
+    public function getTmpDir();
 }
